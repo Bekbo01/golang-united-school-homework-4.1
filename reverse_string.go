499cc306ec821f27f1f4f2118c445f5080a14e15
Bekbolat Kurmetbek <bigboss990930@gmail.com>
2022-05-20 06:05:22 +0000

    solution

--- reverse_string.go
+++ reverse_string.go
@@ -1,6 +1,14 @@
 package reverse_string
 
+import "fmt"
+
 func ReverseString(input string) (output string) {
-	// solution goes here
+	runes := []rune(input)
+
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
+	output = string(runes)
+	fmt.Println(output)
 	return output
 }
